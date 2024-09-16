c29c7853caca7320de2ffe01f7ceb744d47e3ac3
Your Name <you@example.com>
2024-09-16 15:38:26 +0000

    подключение к mysql

--- web/index.php
+++ web/index.php
@@ -1,7 +1,32 @@
 <?php
-    echo '<pre>';
-    echo 'ass</br>';
-    echo 'hole</br>';
-    
-    var_dump($_ENV);
-    echo '</pre>';
+
+$servername = "mysql";
+$username = "myuser";
+$password = "mypassword";
+$dbname = "mydb";
+
+// Создаем соединение
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверяем соединение
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Выполняем запрос
+$sql = "SELECT * FROM mytable";
+$result = $conn->query($sql);
+
+// Выводим результат
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "acccccc: " . $row["a"]. " - b: " . $row["b"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Закрываем соединение
+$conn->close();
+?>    
